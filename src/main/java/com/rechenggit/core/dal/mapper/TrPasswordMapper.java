ad0b1e43551e5b19924ea5cd735f7aa0ffeb54d7
王沅全 <yj.wang@rechengit.com>
2018-09-14 17:41:33 +0800

    密码激活

---
+++ src/main/java/com/rechenggit/core/dal/mapper/TrPasswordMapper.java
@@ -0,0 +1,7 @@
+package com.rechenggit.core.dal.mapper;
+
+import com.rechenggit.core.dal.dataobject.TrPassword;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TrPasswordMapper extends Mapper<TrPassword> {
+}
