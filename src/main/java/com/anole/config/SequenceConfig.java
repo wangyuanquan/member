2bdca87359459d41b85b45e6e98f637883c00214
wangyuanquan <1101699612@qq.com>
2018-11-26 10:44:00 +0800

    初始化

--- src/main/java/com/anole/config/SequenceConfig.java
+++ src/main/java/com/anole/config/SequenceConfig.java
@@ -12,10 +12,14 @@ public class SequenceConfig {
     @Bean(name = "unitySequenceFlushTreadPoolTaskExecutor")
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        threadPoolTaskExecutor.setCorePoolSize(1);
-        threadPoolTaskExecutor.setMaxPoolSize(1);
-        threadPoolTaskExecutor.setQueueCapacity(1000);
-        threadPoolTaskExecutor.setKeepAliveSeconds(300);
+        //线程池维护线程的最少数量
+        threadPoolTaskExecutor.setCorePoolSize(5);
+        //线程池维护线程的最大数量
+        threadPoolTaskExecutor.setMaxPoolSize(10);
+        //缓存队列
+        threadPoolTaskExecutor.setQueueCapacity(100);
+        //允许的空闲时间
+        threadPoolTaskExecutor.setKeepAliveSeconds(100);
         threadPoolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
         return threadPoolTaskExecutor;
     }
